83bd86e5fa688a7b2a823db60dc5bd630939c06c
Daniele Trapani <daniele.trapani@live.it>
2014-10-16 22:43:41 +0200

    Refactored selectText()

--- directives.js
+++ directives.js
@@ -4,27 +4,23 @@ angular.module('faces')
       link: function (scope, element, attrs) {
 
         var selectText = function() {
-          var doc = $document[0],
-            element = this,
-            range, selection;
-          if (doc.body.createTextRange) {
-            range = $document[0].body.createTextRange();
-            range.moveToElementText(element);
-            range.select();
-          } else if ($window.getSelection) {
-            selection = $window.getSelection();
-            range = $document[0].createRange();
-            range.selectNodeContents(element);
-            selection.removeAllRanges();
-            selection.addRange(range);
-          }
+          var doc       = $document[0],
+              elem      = this,
+              selection = $window.getSelection(),
+              range     = doc.createRange();
+
+          range.selectNodeContents(elem);
+          selection.removeAllRanges();
+          selection.addRange(range);
         };
 
         element.on('click', function () {
+          var notif;
+
           selectText.apply(this);
           $document[0].execCommand("Copy");
           $window.getSelection().removeAllRanges();
-          var notif = angular.element('<notify>Copied to clipboard.</notify>');
+          notif = angular.element('<notify>Copied to clipboard.</notify>');
           element.append(notif);
           $compile(notif)(scope);
         })
